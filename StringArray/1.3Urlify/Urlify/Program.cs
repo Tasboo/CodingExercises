32358441f14e0c6bdc58f4ac26c7e6888ffd0ebe
Graham Huck <graham.huck@gmail.com>
2019-02-16 12:46:23 -0600

    Add Urlify

---
+++ StringArray/1.3Urlify/Urlify/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Urlify
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Strings and Arrays 1.3: Urlify");
+            var testString = "The cat had a hat        ";
+            var charArray = testString.ToCharArray();
+            Console.WriteLine($"input url: {testString}");
+            UrlifyChar(charArray);
+            Console.WriteLine($"finished url: {new string(charArray)}");
+        }
+
+        public static void UrlifyChar(char[] url)
+        {
+            Console.WriteLine($"char array length: {url.Length}");
+            int r = url.Length - 1;
+            Console.WriteLine($"r: {r}");
+            int s = r - 2;
+            Console.WriteLine($"s: {s}");
+            while(url[s] == ' '){
+                s--;
+            }
+            while(s >= 0)
+            {
+                if(url[s] != ' ')
+                {
+                    url[r--] = url[s];
+                    Console.WriteLine($"swapped char: {new string(url)}");
+                }
+                else
+                {
+                    url[r--] = '0';
+                    url[r--] = '2';
+                    url[r--] = '%';
+                    Console.WriteLine($"replaced space: {new string(url)}");
+                }
+                s--;
+            }
+        }
+    }
+}
