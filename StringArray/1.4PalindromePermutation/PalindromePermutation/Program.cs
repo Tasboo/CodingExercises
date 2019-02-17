d667a0a21d1059db801b53f6b9d30aef585a5a57
Graham Huck <graham.huck@gmail.com>
2019-02-17 08:12:14 -0600

    Added Palindrome Permutation

---
+++ StringArray/1.4PalindromePermutation/PalindromePermutation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromePermutation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = "Tact Coa";
+            Console.WriteLine($"Input string: \"{input}\"");
+            Console.WriteLine($"Is expected to return true.");
+            Console.WriteLine($"Actual: {IsPalindromePermutation(input)}");
+
+            Console.WriteLine($" ");
+            input = "Tact Coaa";
+            Console.WriteLine($"Input string: \"{input}\"");
+            Console.WriteLine($"Is expected to return false.");
+            Console.WriteLine($"Actual: {IsPalindromePermutation(input)}");
+        }
+
+        public static bool IsPalindromePermutation(string input)
+        {
+            Dictionary<char, bool> dictionary = new Dictionary<char, bool>();
+            var ca = input.ToLower().ToCharArray();
+            var length = ca.Length;
+            for(var i = 0; i < length; i++)
+            {
+                if(ca[i] != ' ')
+                {
+                    if(dictionary.ContainsKey(ca[i]))
+                    {
+                        dictionary[ca[i]] = !dictionary[ca[i]];
+                    }
+                    else
+                    {
+                        dictionary.Add(ca[i], true);
+                    }
+                }
+            }
+            int oddCount = 0;
+            foreach(var d in dictionary)
+            {
+                if(d.Value) oddCount++;
+                if(oddCount > 1)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
